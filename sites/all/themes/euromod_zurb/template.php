769f4acb0bbb5ca165fb76bea903f436b3cb705c
jnears <jnears@essex.ac.uk>
2015-10-12 11:24:03 +0100

    Initial commit

---
+++ sites/all/themes/euromod_zurb/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function euromod_zurb_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function euromod_zurb_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function euromod_zurb_preprocess_node(&$variables) {
+}
---
+++ sites/all/themes/euromod_zurb/theme-settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Implements hook_form_FORM_ID_alter().
+ */
+function euromod_zurb_form_system_theme_settings_alter(&$form, &$form_state) {
+
+}
